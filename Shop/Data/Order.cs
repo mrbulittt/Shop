fac724035fbfd4a9de71c97e716eb61f3155f9e2
Ikhsanovv <ikhsanovbulitt@icloud.com>
2025-10-08 18:57:35 +0300

    add order

---
+++ Shop/Data/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shop.Data;
+
+public partial class Order
+{
+    public int IdOrder { get; set; }
+
+    public int? IdUser { get; set; }
+
+    public DateTime OrderDate { get; set; }
+
+    public decimal? TotalAmount { get; set; }
+
+    public string? Status { get; set; }
+
+    public virtual ICollection<Basket> Baskets { get; set; } = new List<Basket>();
+
+    public virtual User? IdUserNavigation { get; set; }
+}
